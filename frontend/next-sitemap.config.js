bac39d9ee67f721082e0a68bf48f592ff75db3d7
DimaHavr <dimaroztoky@gmail.com>
2023-11-24 06:51:16 +0200

    update

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://shop-gamma-nine.vercel.app/', // FIXME: Change to the production URL
+  siteUrl: 'https://roztox.vercel.app/', // FIXME: Change to the production URL
   generateRobotsTxt: true,
   output: 'export',
 }
